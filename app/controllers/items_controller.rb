9bf179418daa6d312cc86025603366b6ceb7ddd5
Igor <shop.ru.shop@mail.ru>
2020-09-19 12:50:20 +0300

    cha

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
     def index
         @items = Item.all
-        render body: @items.map {|i| "#{i.name}: #{i.price}:"}  
+       # render body: @items.map {|i| "#{i.name}: #{i.price}:"}  
     
   end
 end 
