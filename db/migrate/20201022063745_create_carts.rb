e8348aade4044d611b06f3cd6f1e450e13d191dd
barabas007 <shop.ru.shop@mail.ru>
2021-01-01 19:09:54 +0300

    21 + Orders

--- db/migrate/20201022063745_create_carts.rb
+++ db/migrate/20201022063745_create_carts.rb
@@ -1,8 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :carts do |t|
-      t.integer :user_id
-      t.timestamps
+    def change
+      create_table :carts do |t|
+        t.integer :user_id
+        t.timestamps
+      end
     end
   end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_063745) do
+ActiveRecord::Schema.define(version: 2021_01_01_140914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2020_10_22_063745) do
     t.integer "votes_count", default: 0
     t.index ["name"], name: "index_items_on_name"
     t.index ["price"], name: "index_items_on_price"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
