e5516130f7efe04530e82d66d92168240378fd48
barabas007 <shop.ru.shop@mail.ru>
2021-02-22 20:28:44 +0300

    added factoriies

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,32 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-    it 'validates name' do
-        should validate_presence_of :name 
-    end
+    it { should validate_presence_of :name }
+    it { should validate_numericality_of :price }
+    it { should have_many :positions }
+    it { should have_many :comments } 
+    it { should have_many :carts}
 
-    it 'validates price' do
-        should validate_numericality_of :price
-    end
+    context 'calculates the price' do
+        let!(:item_1) { create :item, price: 10 }
+        let!(:item_2) { create :item, price: 20 }
+        let!(:order)  { create :order }
 
-    it 'has_many'do
-        should have_many :positions
-        should have_many :comments  
-        should have_many :carts   
-    end
-
-    it 'calculates the price' do
-        item1 = Item.new(price: 10)
-        item2 = Item.new(price: 20) 
-
-        order = Order.new
-        order.items << item1
-        order.items << item2
-
-
-        order.calculate_total
-        expect(order.total).to be 30.0
-    end
+        it 'calculates the price' do
+      
+            order.items << item_1
+            order.items << item_2
+    
+    
+            
+            expect(order.calculate_total).to be 30.0
+    end    
+end
 
     #it 'has_one' do 
     #    should have :----
